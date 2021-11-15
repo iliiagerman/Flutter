d356ed752a92ceaffddb16abf5fb5abaa8a2f0e0
iliia Germaniuk <iliia.germaniuk@gmail.com>
2021-11-15 22:12:56 +0200

    add

--- lib/my_container.dart
+++ lib/my_container.dart
@@ -27,3 +27,30 @@ class MyContainer extends StatelessWidget {
     );
   }
 }
+class MyContainer2 extends StatelessWidget {
+  const MyContainer2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Контейнер Теория'),
+      ),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(),
+            color: Colors.amber[600],
+          ),
+          alignment: Alignment.center,
+          width: 300,
+          height: 300,
+          margin: const EdgeInsets.all(0.0),
+          padding: const EdgeInsets.all(0.0),
+          child: const Text('codeandart'),
+        ),
+      ),
+    );
+  }
+}
