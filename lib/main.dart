8ffe5cf81840346567637a74e75e58dcaa3d1dfc
iliia Germaniuk <iliia.germaniuk@gmail.com>
2021-11-26 20:22:11 +0200

    new programm

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_first/my_container.dart';
-import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
-import 'package:flutter_first/my_flexible.dart';
 import 'package:flutter_first/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  debugPaintSizeEnabled = false;
-  return runApp(MyApp());
-}
+
+void main() => runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Quiz App',
+      title: 'Demo App',
       theme: ThemeData(
-          primarySwatch: Colors.deepPurple),
+          primarySwatch: Colors.amber,
+         backgroundColor: Colors.white,
+         textTheme: GoogleFonts.marmeladTextTheme(
+           Theme.of(context).textTheme,
+         ),
+      ),
       home: HomePage(),
     );
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,21 +1,45 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_first/models/Question.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
   @override
-  _HomePageState createStage() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage>{
-  final QuestionData data = Question();
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Тестирование'),
-      ),
-      body: Container(
-        child: Center(
-          child: Text('Здесь что-то будет'),
+      body: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height - 85,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(35),
+                bottomRight: Radius.circular(35),
+              )),
+          child: ListView(
+            padding: const EdgeInsets.all(10.0),
+            children: <Widget>[
+              Container(
+                child: ListTile(
+                  title: Text(
+                    'Освежающие напитки',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    'Больше чем 100 видов напитков',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  trailing: Icon(Icons.panorama_horizontal),
+                ),
+              ),
+              Container(
+                child: Text('Горизонтальный список карточек'),
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text('Каталог коктейлей')
+              ),
+            ],
+          ),
         ),
       ),
     );
